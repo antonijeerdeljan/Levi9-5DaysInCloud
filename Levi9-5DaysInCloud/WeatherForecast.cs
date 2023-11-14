ce3c91f41af9688c6743e1e46b9759f944e38315
Antonije Erdeljan <antonijeerdeljan01@gmail.com>
2023-11-14 12:50:32 +0100

    Add project files.

---
+++ Levi9-5DaysInCloud/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Levi9_5DaysInCloud
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
